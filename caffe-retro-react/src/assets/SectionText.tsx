3ba1dce7f01798d7ea7ddabfac08145aef090e57
Bosseyang <bosse_yang@hotmail.com>
2025-08-18 17:19:01 +0200

    Create SectionText.tsx
    Add section text as object to export into the section component

---
+++ caffe-retro-react/src/assets/SectionText.tsx
@@ -0,0 +1,23 @@
+export interface ISectionText {
+  text1: string;
+  text2: string;
+}
+
+// export const SectionText: ISectionText = {
+
+// }
+
+export const HotSection: ISectionText = {
+  text1: "Hot freshly ground black coffee or a cup of exquisite tea?",
+  text2: "We give you that perfect cup every time.",
+};
+
+export const JuicySection: ISectionText = {
+  text1: "Ripe fruit - freshly squeezed.",
+  text2: "It's as simple as that. Chunky or smooth - it's your choice.",
+};
+
+export const CosySection: ISectionText = {
+  text1: "Hang around. Enjoy the settings.",
+  text2: "Use our fast WiFi. Borrow a newspaper or a novel.",
+};
