649f36dab2362528d95811e96f7fe6a38a118fc6
Bosseyang <bosse_yang@hotmail.com>
2025-08-18 17:19:15 +0200

    Update Textbox.tsx

--- caffe-retro-react/src/components/Textbox.tsx
+++ caffe-retro-react/src/components/Textbox.tsx
@@ -2,8 +2,13 @@ import type { ReactElement } from "react";
 
 interface ITextboxProps {
   attribute: string;
+  text: string;
 }
 
 export default function Textbox(props: ITextboxProps): ReactElement {
-  return <article className={`${props.attribute} text-box`}></article>;
+  return (
+    <article className={`${props.attribute} text-box`}>
+      <p>{props.text}</p>
+    </article>
+  );
 }
