559446488cfe1a6cc63b1d4f44f5db9a40aa5b2f
Bosseyang <bosse_yang@hotmail.com>
2025-08-18 17:19:20 +0200

    Update main.tsx

--- caffe-retro-react/src/main.tsx
+++ caffe-retro-react/src/main.tsx
@@ -3,18 +3,30 @@ import { createRoot } from "react-dom/client";
 import { Header } from "./components/Header";
 import "./css/index.css";
 import Section from "./components/Section";
+import { HotSection, JuicySection, CosySection } from "./assets/SectionText";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Header />
     <main>
-      <Section id="hot" className="hot-section section" imgSrc="/hot.jpg" />
+      <Section
+        id="hot"
+        className="hot-section section"
+        imgSrc="/hot.jpg"
+        sectionText={HotSection}
+      />
       <Section
         id="juicy"
         className="juicy-section section"
         imgSrc="/juicy.jpg"
+        sectionText={JuicySection}
       />
-      <Section id="cosy" className="cosy-section section" imgSrc="/cosy.jpg" />
+      <Section
+        id="cosy"
+        className="cosy-section section"
+        imgSrc="/cosy.jpg"
+        sectionText={CosySection}
+      />
     </main>
   </StrictMode>
 );
