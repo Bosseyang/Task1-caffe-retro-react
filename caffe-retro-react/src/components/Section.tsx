9b65fa2198da95a6ee411673ea870259679a0db9
Bosseyang <bosse_yang@hotmail.com>
2025-08-18 17:19:10 +0200

    Update Section.tsx

--- caffe-retro-react/src/components/Section.tsx
+++ caffe-retro-react/src/components/Section.tsx
@@ -1,25 +1,31 @@
 import type { ReactElement } from "react";
+import type { ISectionText } from "../assets/SectionText";
 import Textbox from "./Textbox";
 
 interface ISectionProps {
   className: string;
   id: string;
   imgSrc: string;
+  sectionText: ISectionText;
 }
 
 export default function Section(props: ISectionProps): ReactElement {
+  const { className, id, imgSrc, sectionText } = props;
   return (
-    <section className={props.className} id={props.id}>
+    <section className={className} id={id}>
       <img
-        src={props.imgSrc}
+        src={imgSrc}
         alt="${props.id} section background image"
         className="background-image"
       />
       <section className="section-container">
-        <h1 className="section-title">{props.id}</h1>
-        <Textbox attribute="red"></Textbox>
-        <Textbox attribute="bottom"></Textbox>
-        <Textbox attribute="black"></Textbox>
+        <h1 className="section-title">{id}</h1>
+        <Textbox attribute="red" text={sectionText.text1} />
+        <Textbox attribute="red bottom" text={sectionText.text2} />
+        <Textbox
+          attribute="black"
+          text="We give you that perfect cup every time."
+        />
       </section>
     </section>
   );
