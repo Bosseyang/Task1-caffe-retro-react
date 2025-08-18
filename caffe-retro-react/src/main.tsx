1baa530ce9ddb860774d0c770476703fffe39abe
Bosseyang <bosse_yang@hotmail.com>
2025-08-18 15:23:31 +0200

    Update main.tsx

--- caffe-retro-react/src/main.tsx
+++ caffe-retro-react/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Header } from "./components/Header";
 import "./css/index.css";
+import Section from "./components/Section";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Header />
+    <main>
+      <Section />
+    </main>
   </StrictMode>
 );
