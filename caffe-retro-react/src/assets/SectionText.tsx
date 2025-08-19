7ea7a8408ad2ad4ce4a358b8e5c445ed6201c917
Bosseyang <bosse_yang@hotmail.com>
2025-08-19 14:01:40 +0200

    Update SectionText.tsx
    Code cleanup

--- caffe-retro-react/src/assets/SectionText.tsx
+++ caffe-retro-react/src/assets/SectionText.tsx
@@ -3,21 +3,17 @@ export interface ISectionText {
   text2: string;
 }
 
-// export const SectionText: ISectionText = {
-
-// }
-
-export const HotSection: ISectionText = {
+export const hotSection: ISectionText = {
   text1: "Hot freshly ground black coffee or a cup of exquisite tea?",
   text2: "We give you that perfect cup every time.",
 };
 
-export const JuicySection: ISectionText = {
+export const juicySection: ISectionText = {
   text1: "Ripe fruit - freshly squeezed.",
   text2: "It's as simple as that. Chunky or smooth - it's your choice.",
 };
 
-export const CosySection: ISectionText = {
+export const cosySection: ISectionText = {
   text1: "Hang around. Enjoy the settings.",
   text2: "Use our fast WiFi. Borrow a newspaper or a novel.",
 };
