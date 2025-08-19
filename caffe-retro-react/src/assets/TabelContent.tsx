c62a7da9b5c4fe3f5ea220a4de7127fd0a4e31eb
Bosseyang <bosse_yang@hotmail.com>
2025-08-19 08:57:44 +0200

    Create TabelContent.tsx

---
+++ caffe-retro-react/src/assets/TabelContent.tsx
@@ -0,0 +1,11 @@
+export interface ITableContent {
+    item: string;
+    currency: string;
+    price: number;
+}
+
+export const TableContent: ITableContent= {
+    {
+        HotTable[]
+    }
+}
