d86ad88d3047ecb17184054b2ac92a55f90f704a
Bosseyang <bosse_yang@hotmail.com>
2025-08-19 08:57:47 +0200

    Create TableComp.tsx

---
+++ caffe-retro-react/src/components/TableComp.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from "react";
+
+interface ITable {
+    item: string;
+    itemCount: number;
+}
+
+export default function TabelComp(props: ITable):ReactElement = {
+    return ( array.forEach(element => {
+        
+    }););
+};
